79d43d200cfea3d9cadf7a988535bd050fb6d487
Bloody <116832305+Radomir-Aksenenko@users.noreply.github.com>
2025-07-24 00:10:14 +0700

    в

--- src/lib/spw.ts
+++ src/lib/spw.ts
@@ -6,5 +6,10 @@ const spw = new SPWMini('cbf8f8bc-3d0f-4182-abc2-7eb415822755', {
   autoinit: false
 });
 
+// Обработчик события инициализации для вывода информации о пользователе
+spw.on('initResponse', user => {
+  console.log(`Logged in as ${user.username} / ${user.minecraftUUID}`);
+});
+
 // Экспортируем экземпляр для использования в других компонентах
 export default spw;
