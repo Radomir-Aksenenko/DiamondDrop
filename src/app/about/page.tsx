81d029909a335d7b1066cd91d16673d614958b49
Bloody <116832305+Radomir-Aksenenko@users.noreply.github.com>
2025-07-29 16:17:41 +0700

    refactor(about): update about page content and styling
    Simplify the about page by removing container div and updating heading text and styles

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,5 @@
 export default function AboutPage() {
   return (
-    <div className="min-h-screen p-6">
-      <h1 className="text-2xl font-bold mb-4 font-unbounded">О проекте</h1>
-      <p>Информация о проекте Diamond Drop</p>
-    </div>
+    <h1 className="text-[24px] font-[500] text-white text-center font-unbounded">Наша команда</h1>
   );
 }
