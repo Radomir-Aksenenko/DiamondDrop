c417b526aa1ff6efb9d519438af4c0c0bcc5072b
Bloody <116832305+Radomir-Aksenenko@users.noreply.github.com>
2025-08-06 15:07:45 +0700

    ИД для бета теста

--- src/lib/spw.ts
+++ src/lib/spw.ts
@@ -1,10 +1,12 @@
 import SPWMini from 'spwmini/client';
 
 // Инициализация SPWMini с указанным ID приложения
-const spw = new SPWMini('cbf8f8bc-3d0f-4182-abc2-7eb415822755', {
+const spw = new SPWMini('31de9a96-221c-4dca-b817-28a3aa5de556', {
   // Отключаем автоинициализацию для работы с Next.js
   autoinit: false
 });
 
 // Экспортируем экземпляр для использования в других компонентах
 export default spw;
+
+// cbf8f8bc-3d0f-4182-abc2-7eb415822755
