f52fc6b4eae24c228a027fd5ca0f45a09a0a3f1e
Radomir <amazerust@mail.ru>
2025-08-22 20:54:03 +0700

    РЕЛИЗ

--- src/lib/spw.ts
+++ src/lib/spw.ts
@@ -1,7 +1,7 @@
 import SPWMini from 'spwmini/client';
 
 // Инициализация SPWMini с указанным ID приложения
-const spw = new SPWMini('31de9a96-221c-4dca-b817-28a3aa5de556', {
+const spw = new SPWMini('cbf8f8bc-3d0f-4182-abc2-7eb415822755', {
   // Отключаем автоинициализацию для работы с Next.js
   autoinit: false
 });
