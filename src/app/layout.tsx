4142bc7e080a3228d9c9bb109e5189068ce66e3a
Radomir <amazerust@mail.ru>
2025-08-26 20:31:10 +0700

    feat(analytics): add Yandex.Metrika tracking to layout
    Implement Yandex.Metrika analytics tracking script in the application layout to monitor user behavior and gather website statistics. Includes both script tag and noscript fallback.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,9 +24,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Yandex.Metrika counter */}
+        <script type="text/javascript" dangerouslySetInnerHTML={{
+          __html: `
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103901262', 'ym');
+            
+            ym(103901262, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+          `
+        }} />
+        {/* /Yandex.Metrika counter */}
+      </head>
       <body
         className={`${unbounded.variable} antialiased text-[#F9F8FC]`}
       >
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/103901262" style={{position: 'absolute', left: '-9999px'}} alt="" />
+          </div>
+        </noscript>
         <SPWProvider>
           <WalletModalProvider>
             <Header />
